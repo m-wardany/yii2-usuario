ff59cb33f3f5ddf9eaec4c15555770f38535a497
muhammad wardany <muhammad.wardany@gmail.com>
2018-11-20 10:44:57 +0200

    Update Facebook.php

--- src/User/AuthClient/Facebook.php
+++ src/User/AuthClient/Facebook.php
@@ -32,6 +32,7 @@ class Facebook extends BaseFacebook implements AuthClientInterface
     public function getUsername()
     {
         /* returns the e-mail as it corresponds with the username */
-        return $this->getEmail();
+        $splitter = explode('@', $this->client->getEmail());
+        return $splitter[0];
     }
 }
